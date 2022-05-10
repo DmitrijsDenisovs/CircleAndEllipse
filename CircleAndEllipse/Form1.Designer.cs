b889b18f67e4b9e07c8137c626e100edb4fe6db2
Deniss Dmitrijevs <dod3005@gmail.com>
2022-05-10 20:22:15 +0300

    Add project files.

---
+++ CircleAndEllipse/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace CircleAndEllipse
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.xLabel = new System.Windows.Forms.Label();
+            this.xTextBox = new System.Windows.Forms.TextBox();
+            this.yLabel = new System.Windows.Forms.Label();
+            this.yTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.x2Label = new System.Windows.Forms.Label();
+            this.x2TextBox = new System.Windows.Forms.TextBox();
+            this.y2Label = new System.Windows.Forms.Label();
+            this.y2TextBox = new System.Windows.Forms.TextBox();
+            this.CircleButton = new System.Windows.Forms.Button();
+            this.EllipseButton = new System.Windows.Forms.Button();
+            this.x3TextBox = new System.Windows.Forms.TextBox();
+            this.y3TextBox = new System.Windows.Forms.TextBox();
+            this.r1Label = new System.Windows.Forms.Label();
+            this.r2Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // xLabel
+            // 
+            this.xLabel.AutoSize = true;
+            this.xLabel.Location = new System.Drawing.Point(679, 15);
+            this.xLabel.Name = "xLabel";
+            this.xLabel.Size = new System.Drawing.Size(14, 15);
+            this.xLabel.TabIndex = 0;
+            this.xLabel.Text = "X";
+            // 
+            // xTextBox
+            // 
+            this.xTextBox.Location = new System.Drawing.Point(699, 12);
+            this.xTextBox.Name = "xTextBox";
+            this.xTextBox.Size = new System.Drawing.Size(89, 23);
+            this.xTextBox.TabIndex = 1;
+            // 
+            // yLabel
+            // 
+            this.yLabel.AutoSize = true;
+            this.yLabel.Location = new System.Drawing.Point(679, 45);
+            this.yLabel.Name = "yLabel";
+            this.yLabel.Size = new System.Drawing.Size(14, 15);
+            this.yLabel.TabIndex = 2;
+            this.yLabel.Text = "Y";
+            // 
+            // yTextBox
+            // 
+            this.yTextBox.Location = new System.Drawing.Point(699, 42);
+            this.yTextBox.Name = "yTextBox";
+            this.yTextBox.Size = new System.Drawing.Size(89, 23);
+            this.yTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // x2Label
+            // 
+            this.x2Label.AutoSize = true;
+            this.x2Label.Location = new System.Drawing.Point(679, 74);
+            this.x2Label.Name = "x2Label";
+            this.x2Label.Size = new System.Drawing.Size(20, 15);
+            this.x2Label.TabIndex = 4;
+            this.x2Label.Text = "X2";
+            // 
+            // x2TextBox
+            // 
+            this.x2TextBox.Location = new System.Drawing.Point(699, 71);
+            this.x2TextBox.Name = "x2TextBox";
+            this.x2TextBox.Size = new System.Drawing.Size(89, 23);
+            this.x2TextBox.TabIndex = 5;
+            // 
+            // y2Label
+            // 
+            this.y2Label.AutoSize = true;
+            this.y2Label.Location = new System.Drawing.Point(679, 101);
+            this.y2Label.Name = "y2Label";
+            this.y2Label.Size = new System.Drawing.Size(20, 15);
+            this.y2Label.TabIndex = 6;
+            this.y2Label.Text = "Y2";
+            // 
+            // y2TextBox
+            // 
+            this.y2TextBox.Location = new System.Drawing.Point(699, 98);
+            this.y2TextBox.Name = "y2TextBox";
+            this.y2TextBox.Size = new System.Drawing.Size(89, 23);
+            this.y2TextBox.TabIndex = 7;
+            // 
+            // CircleButton
+            // 
+            this.CircleButton.Location = new System.Drawing.Point(679, 194);
+            this.CircleButton.Name = "CircleButton";
+            this.CircleButton.Size = new System.Drawing.Size(109, 23);
+            this.CircleButton.TabIndex = 8;
+            this.CircleButton.Text = "Draw a circle";
+            this.CircleButton.UseVisualStyleBackColor = true;
+            this.CircleButton.Click += new System.EventHandler(this.CircleButton_Click);
+            // 
+            // EllipseButton
+            // 
+            this.EllipseButton.Location = new System.Drawing.Point(679, 223);
+            this.EllipseButton.Name = "EllipseButton";
+            this.EllipseButton.Size = new System.Drawing.Size(109, 23);
+            this.EllipseButton.TabIndex = 9;
+            this.EllipseButton.Text = "Draw an ellipse";
+            this.EllipseButton.UseVisualStyleBackColor = true;
+            this.EllipseButton.Click += new System.EventHandler(this.EllipseButton_Click);
+            // 
+            // x3TextBox
+            // 
+            this.x3TextBox.Location = new System.Drawing.Point(699, 128);
+            this.x3TextBox.Name = "x3TextBox";
+            this.x3TextBox.Size = new System.Drawing.Size(89, 23);
+            this.x3TextBox.TabIndex = 10;
+            // 
+            // y3TextBox
+            // 
+            this.y3TextBox.Location = new System.Drawing.Point(699, 157);
+            this.y3TextBox.Name = "y3TextBox";
+            this.y3TextBox.Size = new System.Drawing.Size(89, 23);
+            this.y3TextBox.TabIndex = 11;
+            // 
+            // r1Label
+            // 
+            this.r1Label.AutoSize = true;
+            this.r1Label.Location = new System.Drawing.Point(679, 131);
+            this.r1Label.Name = "r1Label";
+            this.r1Label.Size = new System.Drawing.Size(20, 15);
+            this.r1Label.TabIndex = 12;
+            this.r1Label.Text = "X3";
+            // 
+            // r2Label
+            // 
+            this.r2Label.AutoSize = true;
+            this.r2Label.Location = new System.Drawing.Point(679, 160);
+            this.r2Label.Name = "r2Label";
+            this.r2Label.Size = new System.Drawing.Size(20, 15);
+            this.r2Label.TabIndex = 13;
+            this.r2Label.Text = "Y3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.r2Label);
+            this.Controls.Add(this.r1Label);
+            this.Controls.Add(this.y3TextBox);
+            this.Controls.Add(this.x3TextBox);
+            this.Controls.Add(this.EllipseButton);
+            this.Controls.Add(this.CircleButton);
+            this.Controls.Add(this.y2TextBox);
+            this.Controls.Add(this.y2Label);
+            this.Controls.Add(this.x2TextBox);
+            this.Controls.Add(this.x2Label);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.yTextBox);
+            this.Controls.Add(this.yLabel);
+            this.Controls.Add(this.xTextBox);
+            this.Controls.Add(this.xLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label xLabel;
+        private System.Windows.Forms.TextBox xTextBox;
+        private System.Windows.Forms.Label yLabel;
+        private System.Windows.Forms.TextBox yTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label x2Label;
+        private System.Windows.Forms.TextBox x2TextBox;
+        private System.Windows.Forms.Label y2Label;
+        private System.Windows.Forms.TextBox y2TextBox;
+        private System.Windows.Forms.Button CircleButton;
+        private System.Windows.Forms.Button EllipseButton;
+        private System.Windows.Forms.TextBox x3TextBox;
+        private System.Windows.Forms.TextBox y3TextBox;
+        private System.Windows.Forms.Label r1Label;
+        private System.Windows.Forms.Label r2Label;
+    }
+}
+
---
+++ CircleAndEllipse/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CircleAndEllipse
+{
+    public partial class Form1 : Form
+    {
+        int x;
+        int y;
+        int x2;
+        int y2;
+        int x3;
+        int y3;
+
+        int pointNumber = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CircleButton_Click(object sender, EventArgs e)
+        {
+            if (GetCoords())
+            {
+                int r = (int) Math.Ceiling(Math.Sqrt(Math.Pow(x2 - x, 2) + Math.Pow(y2 - y, 2)));
+                DrawCircle(Color.Red, x, y, r);
+            }
+            else
+            {
+                MessageBox.Show("Coordinates must be positive integer numbers", "Invalid input");
+            }
+        }
+
+        private void EllipseButton_Click(object sender, EventArgs e)
+        {
+            bool valid = GetCoords();
+
+            if (Int32.TryParse(x3TextBox.Text, out x3))
+            {
+                if (x3 < 0)
+                {
+                    valid = false;
+                }
+            }
+            else
+            {
+                valid = false;
+            }
+
+            if (Int32.TryParse(y3TextBox.Text, out y3))
+            {
+                if (y3 < 0)
+                {
+                    valid = false;
+                }
+            }
+            else
+            {
+                valid = false;
+            }
+
+            if (valid)
+            {
+                double r1 = Math.Sqrt(Math.Pow(x3 - x, 2) + Math.Pow(y3 - y, 2));
+                double r2 = Math.Sqrt(Math.Pow(x3 - x2, 2) + Math.Pow(y3 - y2, 2));
+                DrawEllipse(Color.Purple, x, y, x2, y2, r1, r2);
+            }
+            else
+            {
+                MessageBox.Show("Coordinates and radiuses must be positive integer numbers", "Invalid input");
+            }
+        }
+
+        private void DrawPoint(Color color, int x, int y)
+        {
+            SolidBrush brush = new SolidBrush(color);
+
+            this.CreateGraphics().FillRectangle(brush, x, y, 1, 1);
+        }
+
+ 
+
+        private void DrawCircle(Color color, int centerX, int centerY, int radius)
+        {
+            //initialize start of one octant
+            int x = radius;
+
+            int radiusError = 1 - radius;
+
+            for (int y = 0; y < x; ++y)
+            {
+                DrawPoint(color, x + centerX, y + centerY);
+                DrawPoint(color, centerX - x, y + centerY);
+                DrawPoint(color, x + centerX, centerY - y);
+                DrawPoint(color, centerX - x, centerY - y);
+                DrawPoint(color, y + centerX, x + centerY);
+                DrawPoint(color, centerX - y, x + centerY);
+                DrawPoint(color, y + centerX, centerY - x);
+                DrawPoint(color, centerX - y, centerY - x);
+
+                if (radiusError <= 0)
+                {
+                    radiusError += 2 * y + 1;
+                }
+                else
+                {
+                    --x;
+                    radiusError += 2 * y - 2 * x + 1;
+                }
+
+            }
+        }
+
+        private void DrawEllipse(Color color, int x1, int y1, int x2, int y2, double r1, double r2)
+        {
+            List<Tuple<double, double>> coordsToRotate = new List<Tuple<double, double>>();
+            double focalDist = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)) / 2.0;
+            double biggerHalfaxis = (r1 + r2) / 2.0;
+            double eccentricity = focalDist / biggerHalfaxis;
+
+            if (Math.Round(eccentricity, 10) >= 1.0)
+            {
+                MessageBox.Show("Given ellipse does not exist");
+                return;
+            }
+
+            double lesserHalfaxis = biggerHalfaxis * Math.Sqrt(1.0 - Math.Pow(eccentricity, 2));
+
+            double yc = (y1 + y2) / 2.0;
+            double xc = (x1 + x2) / 2.0;
+            double rx = biggerHalfaxis;
+            double ry = lesserHalfaxis; 
+
+            double dx, dy, d1, d2, x, y;
+
+            x = 0;
+            y = ry;
+
+            d1 = (ry * ry) - (rx * rx * ry) + (0.25 * rx * rx);
+            dx = 2 * ry * ry * x;
+            dy = 2 * rx * rx * y;
+
+            while (dx < dy)
+            {
+                coordsToRotate.Add(new Tuple<double, double>((x + xc), (y + yc)));
+                coordsToRotate.Add(new Tuple<double, double>((-x + xc), (y + yc)));
+                coordsToRotate.Add(new Tuple<double, double>((x + xc), (-y + yc)));
+                coordsToRotate.Add(new Tuple<double, double>((-x + xc), (-y + yc)));
+
+                if (d1 < 0)
+                {
+                    ++x;
+                    dx = dx + (2 * ry * ry);
+                    d1 = d1 + dx + (ry * ry);
+                }
+                else
+                {
+                    ++x;
+                    --y;
+                    dx = dx + (2 * ry * ry);
+                    dy = dy - (2 * rx * rx);
+                    d1 = d1 + dx - dy + (ry * ry);
+                }
+            }
+
+            d2 = ((ry * ry) * ((x + 0.5) * (x + 0.5))) + ((rx * rx) * ((y - 1) * (y - 1))) - (rx * rx * ry * ry);
+
+            while (y >= 0)
+            {
+                coordsToRotate.Add(new Tuple<double, double>((x + xc), (y + yc)));
+                coordsToRotate.Add(new Tuple<double, double>((-x + xc), (y + yc)));
+                coordsToRotate.Add(new Tuple<double, double>((x + xc), (-y + yc)));
+                coordsToRotate.Add(new Tuple<double, double>((-x + xc), (-y + yc)));
+
+                if (d2 > 0)
+                {
+                    --y;
+                    dy = dy - (2 * rx * rx);
+                    d2 = d2 + (rx * rx) - dy;
+                }
+                else
+                {
+                    --y;
+                    ++x;
+                    dx = dx + (2 * ry * ry);
+                    dy = dy - (2 * rx * rx);
+                    d2 = d2 + dx - dy + (rx * rx);
+                }
+            }
+            double k = (double) (x2 - x1) / (double)(y2 - y1);
+            double angle = y1 == y2 ? 0 : Math.PI / 2.0 - Math.Atan(k);
+
+            List<Tuple<int, int>> rotatedCoords = RotateCoords(coordsToRotate, angle, xc, yc);
+
+            foreach(Tuple<int, int> coords in rotatedCoords)
+            {
+                DrawPoint(Color.Blue, coords.Item1, coords.Item2);
+            }
+        }
+
+        private List<Tuple<int, int>> RotateCoords(List<Tuple<double, double>> coordsToRotate, double angle, double xc = 0, double yc = 0)
+        {
+            double sine = Math.Sin(angle);
+            double cosine = Math.Cos(angle);
+
+            List<Tuple<int, int>> rotatedCoords = new List<Tuple<int, int>>();
+
+            foreach (Tuple<double, double> coords in coordsToRotate)
+            {
+                double xToUpdate = coords.Item1 - xc;
+                double yToUpdate = coords.Item2 - yc;
+                double newX = xToUpdate * cosine - yToUpdate * sine;
+                double newY = xToUpdate * sine + yToUpdate * cosine;
+                rotatedCoords.Add(new Tuple<int, int>((int)(newX + xc), (int)(newY + yc)));
+            }
+
+            return rotatedCoords;
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            int x = this.PointToClient(Cursor.Position).X;
+            int y = this.PointToClient(Cursor.Position).Y;
+            this.CreateGraphics().DrawRectangle(new Pen(Color.Black, 5), new Rectangle(x, y, 1, 1));
+
+            switch (pointNumber)
+            {
+                default:
+                    break;
+                case 1:
+                    xTextBox.Text = x.ToString();
+                    yTextBox.Text = y.ToString();
+                    break;
+                case 2:
+                    x2TextBox.Text = x.ToString();
+                    y2TextBox.Text = y.ToString();
+                    break;
+                case 3:
+                    x3TextBox.Text = x.ToString();
+                    y3TextBox.Text = y.ToString();
+                    break;
+            }
+         
+
+            if (++pointNumber > 3)
+            {
+                pointNumber = 1;
+            }
+        }
+
+        private bool GetCoords()
+        {
+            bool valid = true;
+
+            if (Int32.TryParse(xTextBox.Text, out x))
+            {
+                if (x < 0)
+                {
+                    valid = false;
+                }
+            }
+            else
+            {
+                valid = false;
+            }
+
+            if (Int32.TryParse(yTextBox.Text, out y))
+            {
+                if (y < 0)
+                {
+                    valid = false;
+                }
+            }
+            else
+            {
+                valid = false;
+            }
+
+            if (Int32.TryParse(x2TextBox.Text, out x2))
+            {
+                if (x2 < 0)
+                {
+                    valid = false;
+                }
+            }
+            else
+            {
+                valid = false;
+            }
+
+
+            if (Int32.TryParse(y2TextBox.Text, out y2))
+            {
+                if (y2 < 0)
+                {
+                    valid = false;
+                }
+            }
+            else
+            {
+                valid = false;
+            }
+            return valid;
+        }
+    }
+}
